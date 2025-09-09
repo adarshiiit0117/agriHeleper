7d1ffa931043d4d176b764890885e1dc1e3ea69e
ADARSH DUBEY <139523805+adarshiiit0117@users.noreply.github.com>
2025-09-10 00:50:33 +0530

    Update SecurityConfig.java

--- agriconnect-backend-master/src/main/java/agriconnect/agriconnect/config/SecurityConfig.java
+++ agriconnect-backend-master/src/main/java/agriconnect/agriconnect/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // React app
+        config.setAllowedOrigins(List.of("https://agri-connecti.vercel.app")); // React app
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allow preflight
         config.setAllowedHeaders(List.of("*")); // Accept all headers
         config.setExposedHeaders(List.of("Authorization")); // Expose JWT to frontend
