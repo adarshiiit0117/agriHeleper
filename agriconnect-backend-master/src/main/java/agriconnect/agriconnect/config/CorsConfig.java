4257a050d1ca329552a71a19e9db6f599683627a
ADARSH DUBEY <139523805+adarshiiit0117@users.noreply.github.com>
2025-09-10 00:49:33 +0530

    Update CorsConfig.java

--- agriconnect-backend-master/src/main/java/agriconnect/agriconnect/config/CorsConfig.java
+++ agriconnect-backend-master/src/main/java/agriconnect/agriconnect/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://agri-connecti.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
